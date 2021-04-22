f8a41d1b90c0eed6b667df648094cda91c65f040
rajita basu <rajitabasu0@gmail.com>
2021-04-22 18:00:03 +0530

    scoring system

--- sketch.js
+++ sketch.js
@@ -5,6 +5,10 @@ var Engine = Matter.Engine,
  
 var particles = [];
 var plinkos = [];
+var divisions = [];
+var ball;
+var count = 0;
+var gameState = "start";
 
 var divisionHeight=300;
 var score =0;
@@ -54,26 +58,52 @@ function setup() {
 function draw() {
   background("black");
   textSize(20)
- //text("Score : "+score,20,30);
+ text("Score : "+score,20,30);
+ fill("white");
+ textSize(35)
+ text("500", 5,550);
+ text("500", 80,550);
+ text("500", 160,550);
+ text("500", 240,550);
+ text("100", 480,550);
+ text("100", 320,550);
+ text("100", 400,550);
+ text("200", 560,550);
+ text("200", 640,550);
+ text("200", 720,550);
+
   Engine.update(engine);
  
+  ground.display();
+  if(gameState==="end"){
+    textSize(100);
+    text("gameOver", 150,250);
+  }
   
-   for (var i = 0; i < plinkos.length; i++) {
-     
-     plinkos[i].display();
-     
-   }
-   if(frameCount%60===0){
-     particles.push(new particle(random(width/2-30, width/2+30), 10,10));
-     score++;
-   }
- 
-  for (var j = 0; j < particles.length; j++) {
-   
-     particles[j].display();
-   }
-   for (var k = 0; k < divisions.length; k++) {
-     
-     divisions[k].display();
-   }
+  for (var i = 0; i < plinkos.length; i++) { 
+    plinkos[i].display();
+   } 
+   if(ball!=null) { 
+     ball.display(); 
+     if (ball.body.position.y>760) {
+        if (ball.body.position.x < 300) {
+           score=score+500; ball=null; 
+           if ( count>= 5) gameState ="end"; 
+          } 
+          else if (ball.body.position.x < 600 && ball.body.position.x > 301 ) {
+             score = score + 100; ball=null; if ( count>= 5) gameState ="end"; 
+            } 
+            else if (ball.body.position.x < 900 && ball.body.position.x > 601 ) { 
+              score = score + 200; ball=null; if ( count>= 5) gameState ="end";
+             }
+             } 
+            } 
+  for (var k = 0; k < divisions.length; k++) {
+      divisions[k].display();
+      } 
+}
+function mousePressed() { 
+  if(gameState!=="end") { 
+    count++;
+    ball=new Ball(mouseX, 10, 10, 10); } 
 }
